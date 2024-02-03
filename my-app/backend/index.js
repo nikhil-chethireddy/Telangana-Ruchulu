7ff2500a25c0fe8c94809014673623ed4ba33948
Nikhil Reddy Chethireddy <157819315+nikhil-chethireddy@users.noreply.github.com>
2024-02-03 19:29:10 +0530

    Update index.js with env

--- my-app/backend/index.js
+++ my-app/backend/index.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const dotenv = require("dotenv").config();
 const connectDatabase = require("./db");
+const cors = require("cors");
 
+const port = process.env.PORT || 5000;
 connectDatabase();
 app.get("/", (req, res) => {
   res.send("Server is Runnung!");
 });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-with, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
